c6e0a88043b6d118ff4bf48f5ea19376750eccf5
tomo1583 <tomo0523fx@yahoo.co.jp>
2025-08-31 10:46:58 +0900

    RegisterRequest修正

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rules\password;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterRequest extends FormRequest
 {
